5790b376e1503ad04a72fa21de2c8385b7fc1a2b
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-14 21:30:39 -0800

    Version: 0.1.1

--- page.php
+++ page.php
@@ -28,5 +28,5 @@ perform their jobs.
                 <?php get_template_part('template-parts/content', 'none'); ?>
         <?php endif; ?>
     </div>
-<?php get_sidebar('page'); ?>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
