0cc38a3e6e6a5ef88825165387087fa5a6874711
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-19 09:36:08 -0700

    updated

--- public/views/footer.php
+++ public/views/footer.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Luthemes ( footer.php )
+ * The footer for the theme.
+ * 
+ * This is the template that displays the footer of the theme which includes
+ * Copyright information, social navigation and others that falls into this
+ * category.
  *
- * @package   Luthemes
+ * @package   White Spektrum
  * @author    Benjamin Lu ( benlumia007@gmail.com )
  * @copyright Copyright (C) 2014-2021. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/luthemes
+ * @link      https://github.com/benlumia007/white-spektrum
  */
 ?>
 	<footer id="colophon" class="site-footer">
@@ -14,7 +18,7 @@
 			<?php
 			printf(
 				// Translators: 1 = Date, 2 = Site Link.
-				esc_html__( 'Copyright &#169; %1$s. %2$s', 'luthemes' ),
+				esc_html__( 'Copyright &#169; %1$s. %2$s', 'white-spektrum' ),
 				absint( date_i18n( 'Y' ) ),
 				Benlumia007\Backdrop\Theme\Site\render_site_link() // phpcs:ignore
 			);
@@ -23,7 +27,7 @@
 			<?php
 			printf(
 				// Translators: 1 = WordPress Link, 2 = Theme Link.
-				esc_html__( 'Powered By %1$s and %2$s', 'luthemes' ),
+				esc_html__( 'Powered By %1$s and %2$s', 'white-spektrum' ),
 				Benlumia007\Backdrop\Theme\Site\render_wp_link(), // phpcs:ignore
 				Benlumia007\Backdrop\Theme\Site\render_theme_link() // phpcs:ignore
 			);
