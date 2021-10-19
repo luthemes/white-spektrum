f4855e999bf1530eb8247c220badb373ef22134a
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-18 22:29:15 -0700

    updated

--- webpack.mix.export.js
+++ webpack.mix.export.js
@@ -22,21 +22,11 @@ let exportPath = 'initiator';
 
 // Theme root-level files to include.
 let files = [
-	'404.php',
-	'archive.php',
-	'author.php',
 	'changelog.md',
-	'comments.php',
-	'footer.php',
 	'functions.php',
-	'header.php',
 	'index.php',
-	'page.php',
-	'readme.md',
 	'readme.txt',
 	'screenshot.png',
-	'search.php',
-	'single.php',
 	'style.css'
 ];
 
@@ -44,7 +34,7 @@ let files = [
 let folders = [
 	'app',
 	'assets',
-	'language',
+	'languages',
 	'vendor'
 ];
 
