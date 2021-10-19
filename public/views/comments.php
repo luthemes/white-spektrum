3b5f1007b2f37c5b474804a28b8a218483638fd6
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-19 09:02:57 -0700

    updated

--- public/views/comments.php
+++ public/views/comments.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * Initiator ( comments.php )
- *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   White Spektrum
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/white-spektrum
  */
-
 ?>
 
 <?php
