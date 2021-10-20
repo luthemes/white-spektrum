e171220357dda4932d4d6d3b61d471c354969d1b
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-20 11:29:24 -0700

    updated

--- public/views/archive.php
+++ public/views/archive.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * The template for displaying archives.
- * 
- * The archive displays categories, tags, authors and many more. 
- * 
+ * Silver Quantum ( archive.php )
+ *
  * @package   White Spektrum
  * @author    Benjamin Lu ( benlumia007@gmail.com )
  * @copyright Copyright (C) 2014-2021. Benjamin Lu
@@ -14,20 +12,26 @@
 <?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
 <?php $engine->display( 'header' ); ?>
 	<section id="content" class="site-content">
-		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
+		<div id="global-layout" class="no-sidebar">
 			<main id="main" class="content-area">
-				<?php
-					if ( have_posts() ) :
-						while ( have_posts() ) : the_post();
-							$engine->display( 'content/archive' );
-						endwhile;
-						the_posts_pagination();
-					else :
-                            $engine->display( 'content/404/none' );
-					endif;
-				?>
+				<?php if ( ! is_front_page() ) { ?>
+					<header class="archive-header">
+						<?php the_archive_title( '<h1 class="archive-title">', '</h1>'); ?>
+						<?php the_archive_description( 'p class="archive-description">', '</p>'); ?>
+					</header>
+				<?php } ?>
+
+				<?php if ( have_posts() ) : ?>
+					<div class="loop">
+						<ul class="grid">
+							<?php while ( have_posts() ) : the_post(); ?>
+								<?php $engine->display( 'content/archive' ); ?>
+							<?php endwhile; ?>
+						</ul>
+						<?php the_posts_pagination(); ?>
+					</div>
+				<?php endif; ?>
 			</main>
-			<?php Benlumia007\Backdrop\Theme\Sidebar\display( 'sidebar', [ 'primary' ] ); ?>
 		</div>
 	</section>
 <?php $engine->display( 'footer' ); ?>
