f9aaf894686e721953e9587307a47fbc6379f45b
Benjamin Lu <benlumia007@users.noreply.github.com>
2024-03-18 18:18:03 +0000

    updated

--- functions.php
+++ functions.php
@@ -1,56 +1,22 @@
 <?php
 /**
- * White Spektrum ( functions.php )
+ * Default functions template
+ *
+ * This file is used to bootstrap the theme.
  *
  * @package   White Spektrum
- * @copyright Copyright (C) 2014-2020. Benjamin Lu
- * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author    Benjamin Lu ( https://benjlu.com )
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2014 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/white-spektrum
  */
 
-/**
- * Table of Content
- *
- * 1.0 - Compatibility Check
- * 2.0 - Backdrop Core
+/** ------------------------------------------------------------------------------------------
+ * Bootstrap the theme.
+ * -------------------------------------------------------------------------------------------
+ * Load the bootstrap files. Note that autoload should happen first so that any classes or
+ * functions are available that we might need.
  */
 
-/**
- * 1.0 - Compatibility Check
- */
-add_action( 'after_switch_theme', function() {
-	if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) || version_compare( PHP_VERSION, '5.6', '<' ) ) {
-		switch_theme( get_option( 'theme_switched' ) );
-
-		add_action( 'admin_notices', function() { ?>
-			<div class="error">
-				<p>
-					<?php if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) ) {
-						printf( 
-							// translators: 1 =  a version string, 2 = current wp version string.
-							esc_html__( 'White Spektrum requires at least WordPress version %1$s. You are currently running %2$s. Please upgrade and try again.', 'white-spektrum' ),
-							'4.9',
-							$GLOBALS['wp_version'],
-						
-						);
-					} elseif ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
-						printf(
-							// translators: 1 =  a version string, 2 = current wp version string.
-							esc_html__( 'White Spektrum requires at least PHP version %1$s. You are currently running %2$s. Please upgrade and try again.', 'white-spektrum' ),
-							'5.6',
-							PHP_VERSION,
-						);
-					} ?>
-				</p>
-			</div>	
-		<?php } );
-	}
-	return false;
-} );
-
-/**
- * 2.0 - Backdrop Core
- */
-if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
-	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
-}
+require_once get_parent_theme_file_path( 'app/bootstrap-autoload.php' );
+require_once get_parent_theme_file_path( 'app/framework.php' );
