0ffb5f29663ddc7b2b9a401bf9ac19f9c4f94868
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-23 22:23:48 +0000

    updated files.

--- config/image-sizes.php
+++ config/image-sizes.php
@@ -6,7 +6,7 @@
  *
  * @package   White Spektrum
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2024 Benjamin Lu
+ * @copyright 2014 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://luthemes.com/portfolio/white-spektrum
  */
@@ -22,10 +22,5 @@
 		'label'  => __( 'Landscape: Large', 'white-spektrum' ),
 		'width'  => 896,
 		'height' => 504
-	],
-	'white-spektrum-landscape-extra-large' => [
-		'label'  => __( 'Landscape: Extra Large', 'white-spektrum' ),
-		'width'  => 1366,
-		'height' => 768
 	],
 ];
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 White Spektrum WordPress Theme
 ================================================================================================
-Contributor: benlumia007
+Contributor: luthemes
 Tags: custom-background, custom-header, right-sidebar
 Requres at least: 4.5
 Tested up to: 4.9.4
-Stable tag: 0.2.9
+Stable tag: 2.0.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
