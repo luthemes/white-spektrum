0455d2b7b19cd36220d431c6fdaae26aabb046ff
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-23 15:14:02 +0000

    initial commit

---
+++ config/image-sizes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Image Sizes Config.
+ *
+ * Defines the image sizes that the theme sets.
+ *
+ * @package   White Spektrum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/white-spektrum
+ */
+
+ return [
+
+	'white-spektrum-landscape-medium' => [
+		'label'  => __( 'Landscape: Medium', 'white-spektrum' ),
+		'width'  => 640,
+		'height' => 360
+	],
+	'white-spektrum-landscape-large' => [
+		'label'  => __( 'Landscape: Large', 'white-spektrum' ),
+		'width'  => 896,
+		'height' => 504
+	],
+	'white-spektrum-landscape-extra-large' => [
+		'label'  => __( 'Landscape: Extra Large', 'white-spektrum' ),
+		'width'  => 1366,
+		'height' => 768
+	],
+];
