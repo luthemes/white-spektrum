c09441be2f56908c4751dbf5a41ee0c58be89a41
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-23 22:13:49 +0000

    initial commit

---
+++ public/views/content/404.php
@@ -0,0 +1,17 @@
+<section id="content" class="site-content">
+	<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'theme_content_global_layout', 'left-sidebar' ) ); ?>">
+		<main id="main" class="content-area">
+			<?php $error->setup(); ?>
+			<article id="post-0" class="page">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php $error->displayTitle(); ?></h1>
+				</header>
+				<div class="entry-content">
+					<?php $error->displayContent(); ?>
+				</div>
+			</article>
+			<?php $error->reset(); ?>
+		</main>
+		<?php Backdrop\View\display( 'sidebar', 'primary', [ 'sidebar' => 'primary' ] ); ?>
+	</div>
+</section>
