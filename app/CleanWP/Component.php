f1f59de129c598a82f849c89c2db638291ff14be
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-23 23:00:45 +0000

    bump to 2.1.1

--- app/CleanWP/Component.php
+++ app/CleanWP/Component.php
@@ -37,10 +37,6 @@ class Component implements Bootable {
 			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 		}
 
-		if ( Options::get( 'disable_toolbar' ) ) {
-			add_filter( 'show_admin_bar', '__return_false' );
-		}
-
 		// WP adds this on `wp_head` with a priority of `10`, which runs
 		// after scripts have been enqueued, so it goes to the footer.
 		if ( Options::get( 'disable_wp_embed' ) ) {
